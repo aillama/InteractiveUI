c71881637292a9e2bd66cae5f322f8eff208b031
Ananya <ananyatheavenger@gmail.com>
2025-07-23 15:58:16 -0500

    Added textfield and button UI

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //this stores the input of the text field later in the code
+    @State private var name: String = ""
+    
     var body: some View {
         VStack {
             Text("What is your name?")
                 .font(.title)
+            
+            //this text field!
+            TextField("Type your name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.purple, width: 1)
+            
+            Button("Submit Name!") {
+                
+            }
+            .padding()
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
         }
         .padding()
     }
