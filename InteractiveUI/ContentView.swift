6313f1823a1763d19a8ecbe49a27d8085372a291
Ananya <ananyatheavenger@gmail.com>
2025-07-23 16:43:57 -0500

    Added second textbox field and added alert functionality when the Submit button is pressed

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -4,14 +4,16 @@
 //
 //  Created by Ananya Gogula on 7/23/25.
 //
-
+//
 import SwiftUI
 
 struct ContentView: View {
     
     //this stores the input of the text field later in the code
     @State private var name: String = ""
+    @State private var age: Int?
     @State private var textTitle = "What is your name?"
+    @State private var showAlert = false
     
     var body: some View {
         VStack {
@@ -23,15 +25,28 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.purple, width: 1)
+            //the .number ensures that the input is a number?? IDK if it works yet
+            TextField("Type your age here...", value: $age, format: .number)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.purple, width: 1)
             
             Button("Submit Name!") {
                 textTitle = "Welcome \(name)!"
+                showAlert = true
             }
             //button formatting
             .padding()
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
+            
+            .alert("We got your submission!", isPresented: $showAlert) {
+                // DO YOU EVEN NEED THIS BRU Button("OK", role: .cancel) { }
+                    }
+
+            
+            
             
         }
         .padding()
@@ -41,3 +56,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
